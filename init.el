d98430dbb252b92da1ee203edfc2a79646596765
Ethan Goan <ethanjgoan@gmail.com>
2018-03-07 14:23:12 +1000

    updated and formatted init.el

--- init.el
+++ init.el
@@ -1,161 +1,138 @@
-;;; init.el --- Prelude's configuration entry point.
-;;
-;; Copyright (c) 2011-2017 Bozhidar Batsov
-;;
-;; Author: Bozhidar Batsov <bozhidar@batsov.com>
-;; URL: http://batsov.com/prelude
-;; Version: 1.0.0
-;; Keywords: convenience
-
-;; This file is not part of GNU Emacs.
-
-;;; Commentary:
-
-;; This file simply sets up the default load path and requires
-;; the various modules defined within Emacs Prelude.
-
-;;; License:
-
-;; This program is free software; you can redistribute it and/or
-;; modify it under the terms of the GNU General Public License
-;; as published by the Free Software Foundation; either version 3
-;; of the License, or (at your option) any later version.
-;;
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-;;
-;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs; see the file COPYING.  If not, write to the
-;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-;; Boston, MA 02110-1301, USA.
-
-;;; Code:
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-;(package-initialize)
-
-(defvar current-user
-  (getenv
-   (if (equal system-type 'windows-nt) "USERNAME" "USER")))
-
-(message "Prelude is powering up... Be patient, Master %s!" current-user)
-
-(when (version< emacs-version "24.4")
-  (error "Prelude requires at least GNU Emacs 24.4, but you're running %s" emacs-version))
-
-;; Always load newest byte code
-(setq load-prefer-newer t)
-
-(defvar prelude-dir (file-name-directory load-file-name)
-  "The root dir of the Emacs Prelude distribution.")
-(defvar prelude-core-dir (expand-file-name "core" prelude-dir)
-  "The home of Prelude's core functionality.")
-(defvar prelude-modules-dir (expand-file-name  "modules" prelude-dir)
-  "This directory houses all of the built-in Prelude modules.")
-(defvar prelude-personal-dir (expand-file-name "personal" prelude-dir)
-  "This directory is for your personal configuration.
-
-Users of Emacs Prelude are encouraged to keep their personal configuration
-changes in this directory.  All Emacs Lisp files there are loaded automatically
-by Prelude.")
-(defvar prelude-personal-preload-dir (expand-file-name "preload" prelude-personal-dir)
-  "This directory is for your personal configuration, that you want loaded before Prelude.")
-(defvar prelude-vendor-dir (expand-file-name "vendor" prelude-dir)
-  "This directory houses packages that are not yet available in ELPA (or MELPA).")
-(defvar prelude-savefile-dir (expand-file-name "savefile" prelude-dir)
-  "This folder stores all the automatically generated save/history-files.")
-(defvar prelude-modules-file (expand-file-name "prelude-modules.el" prelude-dir)
-  "This files contains a list of modules that will be loaded by Prelude.")
-
-(unless (file-exists-p prelude-savefile-dir)
-  (make-directory prelude-savefile-dir))
-
-(defun prelude-add-subfolders-to-load-path (parent-dir)
- "Add all level PARENT-DIR subdirs to the `load-path'."
- (dolist (f (directory-files parent-dir))
-   (let ((name (expand-file-name f parent-dir)))
-     (when (and (file-directory-p name)
-                (not (string-prefix-p "." f)))
-       (add-to-list 'load-path name)
-       (prelude-add-subfolders-to-load-path name)))))
-
-;; add Prelude's directories to Emacs's `load-path'
-(add-to-list 'load-path prelude-core-dir)
-(add-to-list 'load-path prelude-modules-dir)
-(add-to-list 'load-path prelude-vendor-dir)
-(prelude-add-subfolders-to-load-path prelude-vendor-dir)
-
-;; reduce the frequency of garbage collection by making it happen on
-;; each 50MB of allocated data (the default is on every 0.76MB)
-(setq gc-cons-threshold 50000000)
-
-;; warn when opening files bigger than 100MB
-(setq large-file-warning-threshold 100000000)
-
-;; preload the personal settings from `prelude-personal-preload-dir'
-(when (file-exists-p prelude-personal-preload-dir)
-  (message "Loading personal configuration files in %s..." prelude-personal-preload-dir)
-  (mapc 'load (directory-files prelude-personal-preload-dir 't "^[^#\.].*el$")))
-
-(message "Loading Prelude's core...")
-
-;; the core stuff
-(require 'prelude-packages)
-(require 'prelude-custom)  ;; Needs to be loaded before core, editor and ui
-(require 'prelude-ui)
-(require 'prelude-core)
-(require 'prelude-mode)
-(require 'prelude-editor)
-(require 'prelude-global-keybindings)
-
-;; OSX specific settings
-(when (eq system-type 'darwin)
-  (require 'prelude-osx))
-
-(message "Loading Prelude's modules...")
-
-;; the modules
-(if (file-exists-p prelude-modules-file)
-    (load prelude-modules-file)
-  (message "Missing modules file %s" prelude-modules-file)
-  (message "You can get started by copying the bundled example file"))
-
-;; config changes made through the customize UI will be store here
-(setq custom-file (expand-file-name "custom.el" prelude-personal-dir))
-
-;; load the personal settings (this includes `custom-file')
-(when (file-exists-p prelude-personal-dir)
-  (message "Loading personal configuration files in %s..." prelude-personal-dir)
-  (mapc 'load (directory-files prelude-personal-dir 't "^[^#\.].*el$")))
-
-(message "Prelude is ready to do thy bidding, Master %s!" current-user)
-
-(prelude-eval-after-init
- ;; greet the use with some useful tip
- (run-at-time 5 nil 'prelude-tip-of-the-day))
-
-
-
+;; Setting up directories that have additional plugins
+(add-to-list 'load-path "/home/ethan/.emacs.d/plugins")
+(add-to-list 'load-path "/home/ethan/.emacs.d/packages")
 ;;setting package archives
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
-                         ("marmalade" . "https://marmalade-repo.org/packages/")
-                         ("melpa" . "https://melpa.org/packages/")))
+			 ("marmalade" . "https://marmalade-repo.org/packages/")
+			 ("melpa" . "https://melpa.org/packages/")))
 
+
+;; Some global settings
 
-;;my own settings
+;;highlighting parenthesis etc.
+(show-paren-mode 1)
+(global-linum-mode 1)
 ;;enable pretty control mode
 (require 'pp-c-l)           ; Load this library
 (pretty-control-l-mode 1)
-(require 'pp-c-l)
 (setq pp^L-^L-string-function (lambda (win)
-                                (make-string fill-column ?-)))
+				(make-string fill-column ?-)))
+(require 'package)
+(package-initialize)  ;load and activate packages, including auto-complete
+(ac-config-default)
+(global-auto-complete-mode t)
+;;enable column-enforce mode for sorce code modes
+(require 'column-enforce-mode)
+(package-initialize)
+(add-hook 'prog-mode-hook 'column-enforce-mode)
 
-;;Magit
-(global-set-key (kbd "C-x g") 'magit-status)
+
 
+;;org-wiki mode
+(package-initialize)
+(require 'org-wiki)
+(package-initialize)
+(require 'helm-config)
+;;
+(setq org-wiki-location-list
+      '(
+        "~/org/wiki"    ;; First wiki (root directory) is the default. 
+        ))
+;;forcing image size if it is too large
+(setq org-image-actual-width '(600))
+
+;; Initialize first org-wiki-directory or default org-wiki 
+(setq org-wiki-location (car org-wiki-location-list))
+;;now setting up some alias commands for org-wiki mode
+(defalias 'w-i #'org-wiki-index)
+(defalias 'w-in #'org-wiki-insert)
+(defalias 'w-l #'org-wiki-latex)
+(defalias 'w-lin #'org-wiki-insert-latex)
+(defalias 'w-e #'org-wiki-export-html) ;;exports the entire (well most) of the wiki
+(defalias 'w-h #'org-html-export-to-html);;exports a single page
+(defalias 'w-im #'org-display-inline-images)
+
+;;automatically change parent task to DONE when all child tasks are done
+(defun org-summary-todo (n-done n-not-done)
+  "Switch entry to DONE when all subentries are done, to TODO otherwise."
+  (let (org-log-done org-log-states)   ; turn off logging
+    (org-todo (if (= n-not-done 0) "DONE" "TODO"))))
+(add-hook 'org-after-todo-statistics-hook 'org-summary-todo)
+
+;;export checkboxes with pretty prettiness in html
+(setq org-html-checkbox-type 'html)
+
+;;add some more TODO list keywords
+(setq org-todo-keywords
+  '((sequence "TODO" "TOREAD" "FEEDBACK" "INPROGRESS" "WAITING" "VERIFY" "NOTE" "|" "DONE" "DELEGATED" "NOTE")))
+
+;;PYTHON
+;;Indentation highlighting in Python Mode
+;;If not using Prelude then use this modules
+(add-hook 'python-mode-hook 'highlight-indentation-mode)
+(add-hook 'python-mode-hook 'company-mode)
+
+
+;; use IPython shell
+(defun ipython ()
+  (interactive)
+      (term "/usr/bin/ipython"))
+
+(when (require 'cython-mode nil 'no-error)
+  (add-to-list 'auto-mode-alist '("\\.pyx\\'" . cython-mode))
+  (add-to-list 'auto-mode-alist '("\\.pxd\\'" . cython-mode))
+  (add-to-list 'auto-mode-alist '("\\.pxi\\'" . cython-mode)))
+
+
+
+;;settings for MATLAB
+(custom-set-variables
+ '(matlab-shell-command-switches '("-nodesktop -nosplash")))
+(add-hook 'matlab-mode-hook 'auto-complete-mode)
+(setq auto-mode-alist
+    (cons
+     '("\\.m$" . matlab-mode)
+     auto-mode-alist))
+
+
+;;Some of my other custom set variables
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["black" "red3" "ForestGreen" "yellow3" "blue" "magenta3" "DeepSkyBlue" "gray50"])
+ '(custom-enabled-themes (quote (wheatgrass)))
+ '(inhibit-startup-screen t)
+ '(org-startup-folded t)
+ '(org-startup-truncated nil)
+ '(org-wiki-template
+   "#+TITLE: %n
+#+DESCRIPTION:
+#+KEYWORDS:
+#+STARTUP:  content
+#+OPTIONS: \\n:t
+#+OPTIONS: H:3 num:2
+#+HTML_HEAD: <link rel=\"stylesheet\" type=\"text/css\" href=\"http://www.pirilampo.org/styles/readtheorg/css/htmlize.css\"/>
+#+HTML_HEAD: <link rel=\"stylesheet\" type=\"text/css\" href=\"http://www.pirilampo.org/styles/readtheorg/css/readtheorg.css\"/>
+#+HTML_HEAD: <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js\"></script>
+#+HTML_HEAD: <script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js\"></script>
+#+HTML_HEAD: <script type=\"text/javascript\" src=\"http://www.pirilampo.org/styles/lib/js/jquery.stickytableheaders.js\"></script>
+#+HTML_HEAD: <script type=\"text/javascript\" src=\"http://www.pirilampo.org/styles/readtheorg/js/readtheorg.js\"></script>
+
+- [[wiki:index][Index]]
+
+- Related: 
+
+* %n
+"))
 ;;; init.el ends here
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
